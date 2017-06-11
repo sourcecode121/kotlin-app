a32d069b468c7fad46d845b7313845124f92e039
sourcecode121 <sourcecode121@users.noreply.github.com>
2017-06-11 09:52:50 +0530

    Operator overloading

--- app/src/main/java/com/example/kotlinapp/DomainClasses.kt
+++ app/src/main/java/com/example/kotlinapp/DomainClasses.kt
@@ -6,7 +6,13 @@ package com.example.kotlinapp
 class DomainClasses {
 
     data class ForecastList(val city: String, val country: String,
-                            val dailyForecast:List<Forecast>)
+                            val dailyForecast: List<Forecast>) {
+
+        val size: Int
+            get() = dailyForecast.size
+
+        operator fun get(position: Int): Forecast = dailyForecast[position]
+    }
 
     data class Forecast(val date: String, val description: String, val high: Int,
                         val low: Int)
